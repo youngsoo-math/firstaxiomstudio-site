8c1739d5f2eaa0cea2e48a55b5318da9b9a9c8cb
Youngsoo Kim <youngsoo.kim.math@gmail.com>
2026-01-03 12:18:05 -0600

    docs: Remove comment explaining Vite base path configuration.

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,6 @@ import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    // Set the base path to relative so it works on GitHub Pages sub-paths
     base: './',
     build: {
         outDir: 'dist',
